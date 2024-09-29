650433636f46449d996eeab36ca487bd00bf6713
DaZzelr <tanishqsaini872@gmail.com>
2024-09-29 13:14:34 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,52 +1,33 @@
 import sys
-
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
-    if len(sys.argv) < 3:
-        print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
-        exit(1)
-
-    command = sys.argv[1]
-    filename = sys.argv[2]
-
-    if command != "tokenize":
-        print(f"Unknown command: {command}", file=sys.stderr)
-        exit(1)
-
-    with open(filename) as file:
-        file_contents = file.read()
-
-    error = False
-
-    # Uncomment this block to pass the first stage
-    for c in file_contents:
-        if c == "(":
-            print("LEFT_PAREN ( null")
-        elif c == ")":
-            print("RIGHT_PAREN ) null")
-        elif c == "{":
-            print("LEFT_BRACE { null")
-        elif c == "}":
-            print("RIGHT_BRACE } null")
-        elif c == ",":
-            print("COMMA , null")
-        elif c == ".":
-            print("DOT . null")
-        elif c == "*":
-            print("STAR * null")
-        elif c == "+":
-            print("PLUS + null")
-        elif c == "-":
-            print("MINUS - null")
-        elif c == ";":
-            print("SEMICOLON ; null")
-        # Ignore whitespaces (spaces, tabs, newlines)
-        elif c in " \t\n\r":
-            continue
-        else:
-            error = True
+error_code = 0
+def scan(file_contents):
+    global error_code
+    for syntas in file_contents:
+        match syntas:
+            case "(":
+                print("LEFT_PAREN ( null")
+            case ")":
+                print("RIGHT_PAREN ) null")
+            case "{":
+                print("LEFT_BRACE { null")
+            case "}":
+                print("RIGHT_BRACE } null")
+            case "*":
+                print("STAR * null")
+            case ".":
+                print("DOT . null")
+            case ",":
+                print("COMMA , null")
+            case "+":
+                print("PLUS + null")
+            case "-":
+                print("MINUS - null")
+            case ";":
+                print("SEMICOLON ; null")
+            case "/":
+                print("SLASH / null")
+            case _:
+                error_code = 65
                 line_number = (
                     file_contents.count("\n", 0, file_contents.find(syntas)) + 1
                 )
@@ -54,13 +35,27 @@ def main():
                     f"[line {line_number}] Error: Unexpected character: {syntas}",
                     file=sys.stderr,
                 )
-
-    # Print EOF after processing the tokens
-    print("EOF  null")
-    if error:
-        exit(65)
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!", file=sys.stderr)
+    if len(sys.argv) < 3:
+        print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
+        exit(1)
+    command = sys.argv[1]
+    filename = sys.argv[2]
+    if command != "tokenize":
+        print(f"Unknown command: {command}", file=sys.stderr)
+        exit(1)
+    with open(filename) as file:
+        file_contents = file.read()
+    # Uncomment this block to pass the first stage
+    if file_contents:
+        scan(file_contents)
+        print("EOF  null")
+        exit(error_code)
     else:
-        exit(0)
-
+        print(
+            "EOF  null"
+        )  # Placeholder, remove this line when implementing the scanner
 if __name__ == "__main__":
     main()
