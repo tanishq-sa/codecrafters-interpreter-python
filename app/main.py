b158c8c7a9e487f429f5eb3353d1d0e86ef62e39
DaZzelr <tanishqsaini872@gmail.com>
2024-09-29 12:56:17 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,47 +1,59 @@
 import sys
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
+
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
         exit(1)
+
     command = sys.argv[1]
     filename = sys.argv[2]
+
     if command != "tokenize":
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
+
     with open(filename) as file:
         file_contents = file.read()
+
+    error = False
+
     # Uncomment this block to pass the first stage
-    if file_contents:
-        # print("EOF  null")
-        for line in file_contents:
-            for char in line:
-                match char:
-                    case "(":
-                        token = "LEFT_PAREN"
-                    case ")":
-                        token = "RIGHT_PAREN"
-                    case "{":
-                        token = "LEFT_BRACE"
-                    case "}":
-                        token = "RIGHT_BRACE"
-                    case ",":
-                        token = "COMMA"
-                    case ".":
-                        token = "DOT"
-                    case "-":
-                        token = "MINUS"
-                    case "+":
-                        token = "PLUS"
-                    case ";":
-                        token = "SEMICOLON"
-                    case "*":
-                        token = "STAR"
-                    case _:
-                        continue
-                        # token = ""
-                print(f"{token} {char} null")
+    for c in file_contents:
+        if c == "(":
+            print("LEFT_PAREN ( null")
+        elif c == ")":
+            print("RIGHT_PAREN ) null")
+        elif c == "{":
+            print("LEFT_BRACE { null")
+        elif c == "}":
+            print("RIGHT_BRACE } null")
+        elif c == ",":
+            print("COMMA , null")
+        elif c == ".":
+            print("DOT . null")
+        elif c == "*":
+            print("STAR * null")
+        elif c == "+":
+            print("PLUS + null")
+        elif c == "-":
+            print("MINUS - null")
+        elif c == ";":
+            print("SEMICOLON ; null")
+        else:
+            error = True
+            line = file_contents.count("\n", 0, file_contents.find(c)) + 1
+            print(f"[line {line}] Error: Unexpected character '{c}' ", file=sys.stderr)
+
+    # Print EOF after processing the tokens
     print("EOF  null")
+    if error:
+        exit(65)
+    else:
+        exit(0)
+
 if __name__ == "__main__":
     main()
