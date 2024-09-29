4fa1b1d25ddb6b3ded780170f76761abfab3e150
DaZzelr <tanishqsaini872@gmail.com>
2024-09-29 13:39:45 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,107 +1,90 @@
 import sys
-
-error_code = 0
-
-def scan(file_contents):
-    global error_code
-    length = len(file_contents)
-
-    i = 0
-    while i < length:
-        syntas = file_contents[i]
-
-        match syntas:
-            case " ":
-                pass
-            case "\t":
-                pass
-            case "/":
-                if i < length - 1 and file_contents[i + 1] == "/":
-                    i += 1
-                    while i < length and file_contents[i] != "\n":
-                        i += 1
-                else:
-                    print("SLASH / null")
-            case "(":
-                print("LEFT_PAREN ( null")
-            case ")":
-                print("RIGHT_PAREN ) null")
-            case "{":
-                print("LEFT_BRACE { null")
-            case "}":
-                print("RIGHT_BRACE } null")
-            case "*":
-                print("STAR * null")
-            case ".":
-                print("DOT . null")
-            case ",":
-                print("COMMA , null")
-            case "+":
-                print("PLUS + null")
-            case "-":
-                print("MINUS - null")
-            case ";":
-                print("SEMICOLON ; null")
-            case "!":
-                if i < length - 1 and file_contents[i + 1] == "=":
-                    print("BANG_EQUAL != null")
-                    i += 1
-                else:
-                    print("BANG ! null")
-            case "=":
-                if i < length - 1 and file_contents[i + 1] == "=":
-                    print("EQUAL_EQUAL == null")
-                    i += 1 
-                else:
-                    print("EQUAL = null")
-            case "<":
-                if i < length - 1 and file_contents[i + 1] == "=":
-                    print("LESS_EQUAL <= null")
-                    i += 1
-                else:
-                    print("LESS < null")
-            case ">":
-                if i < length - 1 and file_contents[i + 1] == "=":
-                    print("GREATER_EQUAL >= null")
-                    i += 1
-                else:
-                    print("GREATER > null")
-            case _:
-                error_code = 65
-                line_number = file_contents.count("\n", 0, i) + 1
-                print(
-                    f"[line {line_number}] Error: Unexpected character: {syntas}",
-                    file=sys.stderr,
-                )
-
-        i += 1
-
 def main():
-    # Debug print statement
-    print("Logs from your program will appear here!", file=sys.stderr)
-
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    # print("Logs from your program will appear here!", file=sys.stderr)
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
         exit(1)
-
     command = sys.argv[1]
     filename = sys.argv[2]
-
     if command != "tokenize":
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
-
     with open(filename) as file:
         file_contents = file.read()
-
-    # Process file contents if not empty
+    # Uncomment this block to pass the first stage
+    line = 1
+    error = False
+    length = len(file_contents)
+    i = 0
     if file_contents:
-        scan(file_contents)
-        print("EOF  null")
-        exit(error_code)
-    else:
-        print("EOF  null")
-        exit(0)
-
+        while i < length:
+            c = file_contents[i]
+            if c == "\n":
+                line += 1
+            elif c == " " or c == "\r" or c == "\t":
+                pass
+            elif c == "(":
+                print("LEFT_PAREN ( null")
+            elif c == ")":
+                print("RIGHT_PAREN ) null")
+            elif c == "{":
+                print("LEFT_BRACE { null")
+            elif c == "}":
+                print("RIGHT_BRACE } null")
+            elif c == ",":
+                print("COMMA , null")
+            elif c == ";":
+                print("SEMICOLON ; null")
+            elif c == ".":
+                print("DOT . null")
+            elif c == "-":
+                print("MINUS - null")
+            elif c == "+":
+                print("PLUS + null")
+            elif c == "*":
+                print("STAR * null")
+            elif c == "=":
+                if i + 1 < length and file_contents[i + 1] == "=":
+                    i += 1
+                    print("EQUAL_EQUAL == null")
+                else:
+                    print("EQUAL = null")
+            elif c == "!":
+                if i + 1 < length and file_contents[i + 1] == "=":
+                    i += 1
+                    print("BANG_EQUAL != null")
+                else:
+                    print("BANG ! null")
+            elif c == "<":
+                if i + 1 < length and file_contents[i + 1] == "=":
+                    i += 1
+                    print("LESS_EQUAL <= null")
+                else:
+                    print("LESS < null")
+            elif c == ">":
+                if i + 1 < length and file_contents[i + 1] == "=":
+                    i += 1
+                    print("GREATER_EQUAL >= null")
+                else:
+                    print("GREATER > null")
+            elif c == "/":
+                if i + 1 < length and file_contents[i + 1] == "/":
+                    i += 1
+                    while i < length and file_contents[i] != "\n":
+                        i += 1
+                    # break
+                else:
+                    print("SLASH / null")
+            else:
+                error = True
+                print(
+                    f"[line {line}] Error: Unexpected character: {c}", file=sys.stderr
+                )
+            i += 1
+    print("EOF  null")
+    if error:
+        exit(65)
+    exit(0)
 if __name__ == "__main__":
     main()
